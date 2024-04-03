466e1c5960809af27cb8aef2ab54ec708697e038
Vladyslav Hordiienko <hordivla@fel.cvut.cz>
2024-04-03 22:36:09 +0200

    Adding to render v.3

--- src/main/java/cz/cvut/ear/privatelib/config/WebSequrityConfig.java
+++ src/main/java/cz/cvut/ear/privatelib/config/WebSequrityConfig.java
@@ -1,3 +1,83 @@
+//package cz.cvut.ear.privatelib.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//
+//import javax.annotation.Resource;
+//import java.util.Arrays;
+//
+//
+//@Configuration
+//@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class WebSequrityConfig extends WebSecurityConfigurerAdapter{
+//    @Resource(name = "userService")
+//    private UserDetailsService userDetailsService;
+//
+//    @Autowired
+//    private UnauthorizedEntryPoint unauthorizedEntryPoint;
+//
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Or whatever origin your React app is served from
+//        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+//        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+//        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
+//        configuration.setAllowCredentials(true);
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
+//    @Override
+//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.cors().and().csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/users/authenticate", "/users/**", "/api/titles/**", "/api/authors/**", "/api/genres/**", "/login.html", "/mainpage.html", "/favicon.ico", "/try.html", "/home.png", "/user.png", "/index.html", "/").permitAll()
+//                .anyRequest().authenticated()
+//                .and()
+//                .exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint).and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//
+//        http.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+//    }
+//
+//    @Bean
+//    public BCryptPasswordEncoder encoder(){
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    @Override
+//    @Bean
+//    public AuthenticationManager authenticationManagerBean() throws Exception {
+//        return super.authenticationManagerBean();
+//    }
+//
+//    @Bean
+//    public JwtAuthenticationFilter authenticationTokenFilterBean() throws Exception {
+//        return new JwtAuthenticationFilter();
+//    }
+//}
 package cz.cvut.ear.privatelib.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +99,12 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import javax.annotation.Resource;
 import java.util.Arrays;
-
+import java.util.Collections;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-public class WebSequrityConfig extends WebSecurityConfigurerAdapter{
+public class WebSequrityConfig extends WebSecurityConfigurerAdapter {
     @Resource(name = "userService")
     private UserDetailsService userDetailsService;
 
@@ -34,7 +114,7 @@ public class WebSequrityConfig extends WebSecurityConfigurerAdapter{
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Or whatever origin your React app is served from
+        configuration.setAllowedOrigins(Collections.singletonList("*")); // Adjust this if necessary for production
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
         configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
@@ -44,19 +124,22 @@ public class WebSequrityConfig extends WebSecurityConfigurerAdapter{
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
+        auth.userDetailsService(userDetailsService).passwordEncoder(encoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/users/authenticate", "/users/**", "/api/titles/**", "/api/authors/**", "/api/genres/**", "/login.html", "/mainpage.html", "/favicon.ico", "/try.html", "/home.png", "/user.png", "/index.html", "/").permitAll()
+                .antMatchers("/", "/index.html", "/static/**", "/favicon.ico", "/manifest.json", "/login.html", "/mainpage.html", "/home.png", "/user.png").permitAll() // Permit all static resources
+                .antMatchers("/users/authenticate", "/users/**", "/api/titles/**", "/api/authors/**", "/api/genres/**").permitAll() // Permit all API requests that should be publicly accessible
                 .anyRequest().authenticated()
                 .and()
-                .exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint).and()
+                .exceptionHandling().authenticationEntryPoint(unauthorizedEntryPoint)
+                .and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
         http.addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
@@ -78,3 +161,4 @@ public class WebSequrityConfig extends WebSecurityConfigurerAdapter{
         return new JwtAuthenticationFilter();
     }
 }
+
